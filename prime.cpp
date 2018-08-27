7bee275470ca0df5ee2dbd878f62e23b8f3909b4
mkchauhan326 <mkchauhan326@gmail.com>
2018-08-27 23:22:01 +0530

    prime.cpp added to print prime number.

---
+++ prime.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+bool isPrim(int x)
+{
+	if(x<=1)
+		return 0;
+
+	for(int i=2;i<=sqrt(x);i++)
+	{
+		if(x%i==0)
+			return 0;
+	}
+
+	return 1;
+}
+
+int main()
+{
+	for(int i=0;i<20;i++)
+	{
+		if(isPrim(i))
+			cout<<i<<" ";
+	}
+	cout<<"\n";
+	return 0;
+}
