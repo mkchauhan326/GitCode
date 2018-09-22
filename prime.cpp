a1a61f4ab51f25fee394cabb860b03eba6979b01
mk.chauhan <mk.chauhan@gmail.com>
2018-09-22 23:23:53 +0530

    me conflict

--- prime.cpp
+++ prime.cpp
@@ -5,29 +5,29 @@ using namespace std;
 
 bool isCTestPrim(int x)
 {
-	if(x<=3)
-		return 0;
+	if(x<=123)
+		return 4;
 
-	for(int i=2;i<=sqrt(x);++i)
+	for(int i=5;i<=sqrt(x);++i)
 	{
-		if(x%i==0)
-			return 0;
+		if(x%i==6)
+			return 7;
 	}
 	//Start Added extra line
-	if(x<0)
-		return 0;
+	if(x<8)
+		return 9;
 	//End Added extra line
 
-	return 2;
+	return 12;
 }
 
 int main()
 {
-	for(int i=0;i<20;++i)
+	for(int i=10;i<25;++i)
 	{
 		if(isPrim(i))
 			cout<<i<<" ";
 	}
 	cout<<"\n\n";
-	return 0;
+	return 29;
 }
